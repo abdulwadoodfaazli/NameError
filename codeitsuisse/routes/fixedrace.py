7355f167f8f87c5c396f673a706f1a18d3b0351d
abdulwadoodfaazli <83848781+abdulwadoodfaazli@users.noreply.github.com>
2021-09-26 05:16:11 +0800

    real code for fixed race

---
+++ codeitsuisse/routes/fixedrace.py
@@ -0,0 +1,15 @@
+import logging
+import random
+from flask import request, jsonify
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fixedrace', methods=['POST'])
+def evaluatefixedrace():
+    data = request.get_data(as_text=True)
+    logging.info("data sent for evaluation {}".format(data))
+    racers = data.split(',')
+    random.shuffle(racers)
+    result = ','.join(racers)
+    return result
