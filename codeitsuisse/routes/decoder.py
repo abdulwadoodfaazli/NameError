f6ad24d49f60578a4f0b97618aa2b19d33121a7b
abdulwadoodfaazli <83848781+abdulwadoodfaazli@users.noreply.github.com>
2021-09-26 05:19:56 +0800

    created decoder

---
+++ codeitsuisse/routes/decoder.py
@@ -0,0 +1,15 @@
+import logging
+import random
+from flask import request, jsonify
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@approute('/decoder', methods=['POST'])
+
+def getAnswer():
+    info = request.get_data(as_text=True)
+    possible = info['possible_values']
+    num = info['num_slots']
+    guess = random.sample(possible, num)
+    return guess
