1ce54d2ee41bdcf692234c2f0fbc3b162f5d1496
abdulwadoodfaazli <83848781+abdulwadoodfaazli@users.noreply.github.com>
2021-09-26 05:20:40 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.fixedrace
+import codeitsuisse.routes.Asteroid
+import codeitsuisse.routes.decoder
 
 
 
