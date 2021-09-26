d8a19db6bca49b5db563c3fb65704142e1863e15
abdulwadoodfaazli <83848781+abdulwadoodfaazli@users.noreply.github.com>
2021-09-26 09:23:38 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,11 +1,18 @@
    
 from flask import Flask
 app = Flask(__name__)
+#import codeitsuisse.routes.square
+#import codeitsuisse.routes.fixedrace
+#import codeitsuisse.routes.Asteroid
+#import codeitsuisse.routes.decoder
 import codeitsuisse.routes.square
-import codeitsuisse.routes.fixedrace
-import codeitsuisse.routes.Asteroid
-import codeitsuisse.routes.decoder
-
+import codeitsuisse.routes.asteroid
+import codeitsuisse.routes.options_optimization
+import codeitsuisse.routes.fixed_race
+import codeitsuisse.routes.decode
+import codeitsuisse.routes.cipher
+import codeitsuisse.routes.stig_one
+# import the file that we have just created
 
 
 
